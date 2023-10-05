cc563b0686d059b162c238c2412ae54babf829a7
Divy484 <98522523+Divy484@users.noreply.github.com>
2023-10-05 08:41:27 +0530

    Practical-9

--- app/src/main/java/com/example/mad_practical_9_21012011072/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012011072/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.example.mad_practical_9_21012011072
 
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
